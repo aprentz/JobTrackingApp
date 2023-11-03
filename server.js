be4d956690b7ca19053f23746f3d1b2f783a65b0
AlexEatsBurgers <in.the_eye@hotmail.com>
2023-11-03 18:54:33 +1100

    commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ dotenv.config()
 import morgan from "morgan"
 import express from "express"
 import mongoose from 'mongoose'
-import {dirname} from 'path'
+import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import path from 'path'
 import cloudinary from 'cloudinary';
@@ -23,9 +23,9 @@ cloudinary.config({
    cloud_name: process.env.CLOUD_NAME,
    api_key: process.env.CLOUD_API_KEY,
    api_secret: process.env.CLOUD_API_SECRET,
- });
+});
 
- 
+
 // EXPRESS SERVER
 const app = express()
 const port = process.env.PORT || 5100
@@ -43,6 +43,10 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/jobs', authenticateUser, jobRouter)
 app.use('/api/v1/users', authenticateUser, userRouter)
 app.use('/api/v1/auth', authRouter)
+
+app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, './public', 'index.html'))
+})
 
 // ERROR HANDLING
 app.use("*", (req, res) => {
